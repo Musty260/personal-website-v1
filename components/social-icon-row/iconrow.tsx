8ae1f1b13c23b76d18717777d951e6bef9668a6a
Musty260 <mustyco260@gmail.com>
2021-07-13 11:12:54 +0100

    Clean iconrow component

--- components/social-icon-row/iconrow.tsx
+++ components/social-icon-row/iconrow.tsx
@@ -1,21 +1,13 @@
-import { useEffect, useState } from "react";
-
 import Email from "../social-icons/email";
 import GitHub from "../social-icons/github";
 import LinkedIn from "../social-icons/linkedin";
-import Twitter from "../social-icons/twitter";
 
 type IconProps = {
     theme: string | undefined
 }
 
 export default function IconRow({theme}: IconProps) {
-    const [mounted, setMounted] = useState(false)
 
-    useEffect(() => setMounted(true), [])
-  
-    if (!mounted) return null
-    
     return (
         <>
             <a href="mailto:mustyco260@gmail.com" target="_blank" rel="noreferrer" style={{ WebkitTapHighlightColor: "transparent" }} className="odd:mt-24 md:odd:mt-0">
