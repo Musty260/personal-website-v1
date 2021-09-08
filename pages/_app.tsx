866b69390448cd33398f5fccff088647f4386e43
Musty260 <mustyco260@gmail.com>
2021-09-08 20:42:42 +0100

    add css stylesheets, rename page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,18 @@ import "tailwindcss/tailwind.css";
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from "next-themes";
 
+// core styles shared by all of react-notion-x (required)
+import 'react-notion-x/src/styles.css'
+
+// used for code syntax highlighting (optional)
+import 'prismjs/themes/prism-tomorrow.css'
+
+// used for collection views (optional)
+import 'rc-dropdown/assets/index.css'
+
+// used for rendering equations (optional)
+import 'katex/dist/katex.min.css'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark">
