dabd9836b384687b31b93f3690877d2d71b74041
Musty260 <mustyco260@gmail.com>
2021-09-08 19:30:53 +0100

    add page

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/dnd',
+        destination: 'https://musty.notion.site/musty/D-D-gaming-bfdcf88c1f7446879465480606b3ef8b',
+      },
+    ]
+  },
   reactStrictMode: true,
 }
